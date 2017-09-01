1db471cfc457c96bce4d38b549a8e1466941815e
Łukasz Woźniak <lukasz.wozniak@infullmobile.com>
2017-09-01 13:27:45 +0200

    Initial commit

---
+++ feature/src/main/java/com/infullmobile/hackatoninstant/feature/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.infullmobile.hackatoninstant.feature
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
+}
