1a01062e08d4d2807b39594235609641cc4e9323
Andrii Kovalchuk <tieorange@gmail.com>
2017-09-01 15:00:06 +0200

    Added idea entity

---
+++ feature/src/main/java/com/infullmobile/hackatoninstant/feature/data/IdeaEntity.kt
@@ -0,0 +1,27 @@
+package com.infullmobile.hackatoninstant.feature.data
+
+import com.google.gson.annotations.SerializedName
+
+data class IdeaEntity(
+
+	@field:SerializedName("Team @ September Hackathon (CURRENT)")
+	val teamSeptemberHackathonCURRENT: String? = null,
+
+	@field:SerializedName("Idea")
+	val idea: String? = null,
+
+	@field:SerializedName("Team @ July Hackathon")
+	val teamJulyHackathon: String? = null,
+
+	@field:SerializedName("Kal")
+	val kal: String? = null,
+
+	@field:SerializedName("Team @ June Hackathon")
+	val teamJuneHackathon: String? = null,
+
+	@field:SerializedName("Team @ August Hackathon")
+	val teamAugustHackathon: String? = null,
+
+	@field:SerializedName("Team @ May Hackathon")
+	val teamMayHackathon: String? = null
+)
